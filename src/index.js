1e086c69c60e55c3e46e77b11013cd66ce31678d
surui1 <surui1@xiaomi.com>
2016-10-26 16:23:55 +0800

    table

--- src/index.js
+++ src/index.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import './index.css';
+import 'antd/dist/antd.min.css'
+import {Table} from 'antd'
+import request from 'superagent'
+
+var api = 'http://101.200.129.112:9527/react1/student/'
+
+const colnums = [
+    {title:'id',dataIndex:'id'},
+    {title:'name',dataIndex:'name'},
+    {title:'age',dataIndex:'age'},
+    {title:'sex',dataIndex:'sex'},
+    {title:'single',dataIndex:'single',render:(single)=><span>{single?'单身狗':'恩爱狗'}</span>}
+
+]
+
+var App = React.createClass({
+    getInitialState(){
+        return {
+            items :[]
+        }
+    },
+    render(){
+        return (
+            <div>
+                <h3>ryan</h3>
+                <Table dataSource={this.state.items} columns={colnums}/>
+            </div>
+        )
+    },
+    componentDidMount(){
+        var that = this
+        request
+            .get(api)
+            .end(function (err, res) {
+                console.log(res.body)
+                res.body = res.body.map(function (obj) {
+                    obj.key = obj.id
+                    return obj
+                })
+                that.setState({
+                    items:res.body
+                })
+            })
+    }
+})
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
